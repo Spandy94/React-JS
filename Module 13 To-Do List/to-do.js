34445073f40e73100114787e01474f350036b7c5
Spandy94 <pupul.spandan94@gmail.com>
2021-08-27 08:48:45 +0530

    todo2

---
+++ Module 13 To-Do List/to-do.js
@@ -0,0 +1,10 @@
+function addMore(){
+
+    document.getElementById('error').innerHTML="";
+    let name = document.getElementById('name').value;
+    if(name==''){
+        document.getElementById('error').innerHTML="Please enter value";
+    }else{
+
+    }
+}
